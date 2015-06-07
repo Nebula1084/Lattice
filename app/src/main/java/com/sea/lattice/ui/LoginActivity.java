045e6d6f43300c8ee3722aeb8fa9197aa2b139eb
Sea <hjw_redeption@outlook.com>
2015-06-07 01:39:38 -1200

    working on login and register

--- app/src/main/java/com/sea/lattice/ui/LoginActivity.java
+++ app/src/main/java/com/sea/lattice/ui/LoginActivity.java
@@ -5,7 +5,6 @@ import android.app.AlertDialog;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
 
@@ -20,6 +19,7 @@ import com.sea.lattice.entity.User;
  * Created by Sea on 6/2/2015.
  */
 public class LoginActivity extends Activity {
+    private Integer flag;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -51,25 +51,38 @@ public class LoginActivity extends Activity {
         final EditText login_account = (EditText) findViewById(R.id.login_account);
         final EditText login_password = (EditText) findViewById(R.id.login_password);
 
+        flag = Integer.valueOf(AvosConnection.FLAG_CONNECTING);
         builder.setTitle("登陆");
         builder.setMessage("正在登陆");
         builder.setCancelable(false);
         builder.setPositiveButton("确认", new DialogInterface.OnClickListener() {
             @Override
             public void onClick(DialogInterface dialog, int which) {
-                dialog.dismiss();
+                if (flag == AvosConnection.FLAG_DONE)
+                    dialog.dismiss();
             }
         });
         final AlertDialog dialog = builder.create();
         dialog.show();
 
+        if (login_account.getText().length()==0){
+            dialog.setMessage("账号不能为空");
+            return;
+        }
+        if (login_password.getText().length()==0){
+            dialog.setMessage("密码不能为空");
+            return;
+        }
+
         User user = new User();
         user.logInInBackground(login_account.getText().toString(), login_password.getText().toString(), new LogInCallback<AVUser>() {
             @Override
             public void done(AVUser avUser, AVException e) {
+                flag = AvosConnection.FLAG_DONE;
                 if (avUser != null){
                     dialog.dismiss();
                     startActivity(new Intent(LoginActivity.this, MainActivity.class));
+                    finish();
                 } else {
                     dialog.setMessage(e.getMessage());
                 }
