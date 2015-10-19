8aa10a102d339306095985f3596bd1d8d743e956
Sea <hjw_redeption@outlook.com>
2015-10-19 00:41:27 -1200

    Line Diagram has been finished

--- app/src/main/java/com/sea/lattice/ui/StatisticActivity.java
+++ app/src/main/java/com/sea/lattice/ui/StatisticActivity.java
@@ -17,12 +17,14 @@ import android.widget.Spinner;
 import android.widget.TextView;
 
 import com.sea.lattice.R;
+import com.sea.lattice.chart.Line;
 import com.sea.lattice.chart.Pie;
 import com.sea.lattice.content.BehaviorMeta;
 import com.sea.lattice.content.DirectoryMeta;
 
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
+import java.util.Date;
 import java.util.Locale;
 
 /**
@@ -31,12 +33,15 @@ import java.util.Locale;
 public class StatisticActivity extends AppCompatActivity {
     private LinearLayout layout;
     private Pie pie;
+    private Line line;
     private Spinner statistics_chart, statistics_range;
     private GestureDetector gestureDetector;
     private TextView act_stt_date;
     private ContentResolver contentResolver;
     private Calendar calendar;
     private Cursor mCursor;
+    Calendar front;
+    Calendar rear;
 
     final private static int forWeek = 0;
     final private static int forMonth = 1;
@@ -57,6 +62,7 @@ public class StatisticActivity extends AppCompatActivity {
         layout = (LinearLayout) findViewById(R.id.chart_container);
         act_stt_date = (TextView) findViewById(R.id.act_stt_date);
         pie = new Pie(this, layout, mDisplay.getWidth(), mDisplay.getHeight());
+        line = new Line(this, layout, mDisplay.getWidth(), mDisplay.getHeight());
         Toolbar toolbar = (Toolbar) findViewById(R.id.statistics_bar);
         statistics_chart = (Spinner) findViewById(R.id.statistics_chart);
         statistics_range = (Spinner) findViewById(R.id.statistics_range);
@@ -142,8 +148,8 @@ public class StatisticActivity extends AppCompatActivity {
 
     private void showDate(int select) {
         SimpleDateFormat format;
-        Calendar front = (Calendar) calendar.clone();
-        Calendar rear = (Calendar) calendar.clone();
+        front = (Calendar) calendar.clone();
+        rear = (Calendar) calendar.clone();
         front.set(Calendar.HOUR_OF_DAY, 0);
         front.set(Calendar.MINUTE, 0);
         front.set(Calendar.SECOND, 0);
@@ -180,6 +186,7 @@ public class StatisticActivity extends AppCompatActivity {
 
     private void showChart(int select) {
         int b = 0, ob = 0, w = 0, ow = 0;
+        double[] ab, aob, aw, aow;
         switch (select) {
             case forPie:
                 if (mCursor.moveToFirst()) {
@@ -204,6 +211,55 @@ public class StatisticActivity extends AppCompatActivity {
                 pie.paint(b, ob, w, ow);
                 break;
             case forLine:
+                int duration = 0;
+                switch (statistics_range.getSelectedItemPosition()) {
+                    case forWeek:
+                        duration = 7;
+                        break;
+                    case forMonth:
+                        duration = rear.getActualMaximum(Calendar.DATE);
+                        break;
+                    case forYear:
+                        duration = 12;
+                        break;
+                }
+                ab = new double[duration];
+                aob = new double[duration];
+                aw = new double[duration];
+                aow = new double[duration];
+                if (mCursor.moveToFirst()) {
+                    do {
+                        int category = mCursor.getInt(mCursor.getColumnIndex(BehaviorMeta.CATEGORY));
+                        Date date = new Date(mCursor.getLong(mCursor.getColumnIndex(BehaviorMeta.DATE)) - front.getTimeInMillis());
+                        int index = 0;
+                        switch (statistics_range.getSelectedItemPosition()) {
+                            case forWeek:
+                                index = date.getDate()-1;
+                                break;
+                            case forMonth:
+                                index = date.getDate()-1;
+                                break;
+                            case forYear:
+                                index = date.getMonth();
+                                break;
+                        }
+                        switch (category) {
+                            case BehaviorMeta.WHITE_BEHAVIOR:
+                                aw[index]++;
+                                break;
+                            case BehaviorMeta.WHITE_COUNTER:
+                                aow[index]++;
+                                break;
+                            case BehaviorMeta.BLACK_BEHAVIOR:
+                                ab[index]++;
+                                break;
+                            case BehaviorMeta.BALCK_COUNTER:
+                                aob[index]++;
+                                break;
+                        }
+                    } while (mCursor.moveToNext());
+                }
+                line.paint(ab, aob, aw, aow);
                 break;
         }
     }
